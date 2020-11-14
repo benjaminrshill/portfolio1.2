673facb27b2384b3d42bcf8adc3bb8d075ae8f55
brownspunbear <benjaminrshill@hotmail.com>
2020-11-14 22:17:04 +0000

    Sections, JS click/keyups, media queries

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,47 @@
-
 const navButton = document.querySelector('nav button');
 const navItems = document.querySelector('nav > div');
+const sections = Array.from(document.querySelectorAll('section'));
+const itemBodies = Array.from(document.querySelectorAll('.itemBody'));
 
-function openMenu() {
-    navItems.classList.add('showHide');
-    setTimeout(() => {
-        navItems.classList.add('rollOut');
-    }, 200);
+let insideSection = false;
+
+function toggleSections() {
+    itemBodies.forEach(item => {
+        item.classList.remove('showHide');
+    });
 }
-function closeMenu() {
-    navItems.classList.remove('rollOut');
-    setTimeout(() => {
-        navItems.classList.remove('showHide');
-    }, 500);
+
+function showSection(i) {
+    itemBodies[i].classList.toggle('showHide');
 }
 
 navButton.addEventListener('click', function() {
-    navItems.classList.contains('showHide') ? closeMenu() : openMenu();
+    navItems.classList.toggle('showHide');
+});
+
+sections.forEach((section, i) => {
+    console.log(i+3);
+    section.setAttribute('tabindex', `${i+3}`);
+    section.addEventListener('click', function() {
+        toggleSections();
+        showSection(i);
+    });
+    section.addEventListener('keyup', function(e) {
+        e.key === 'Enter' || e.key === 'Escape' ? toggleSections() : null;
+        e.key === 'Enter' ? showSection(i) : null;
+    });
+    section.addEventListener('mouseenter', function () {
+        insideSection = true;
+    });
+    section.addEventListener('mouseleave', function () {
+        insideSection = false;
+    });
+});
+
+document.addEventListener('click', function(event) {
+    if (!insideSection) {
+        itemBodies.forEach(item => {
+            item.classList.remove('showHide');
+        });
+    }
 });
--- app/js/dist/app.js
+++ app/js/dist/app.js
@@ -2,22 +2,46 @@
 
 var navButton = document.querySelector('nav button');
 var navItems = document.querySelector('nav > div');
+var sections = Array.from(document.querySelectorAll('section'));
+var itemBodies = Array.from(document.querySelectorAll('.itemBody'));
+var insideSection = false;
 
-function openMenu() {
-  navItems.classList.add('showHide');
-  setTimeout(function () {
-    navItems.classList.add('rollOut');
-  }, 200);
+function toggleSections() {
+  itemBodies.forEach(function (item) {
+    item.classList.remove('showHide');
+  });
 }
 
-function closeMenu() {
-  navItems.classList.remove('rollOut');
-  setTimeout(function () {
-    navItems.classList.remove('showHide');
-  }, 500);
+function showSection(i) {
+  itemBodies[i].classList.toggle('showHide');
 }
 
 navButton.addEventListener('click', function () {
-  navItems.classList.contains('showHide') ? closeMenu() : openMenu();
+  navItems.classList.toggle('showHide');
+});
+sections.forEach(function (section, i) {
+  console.log(i + 3);
+  section.setAttribute('tabindex', "".concat(i + 3));
+  section.addEventListener('click', function () {
+    toggleSections();
+    showSection(i);
+  });
+  section.addEventListener('keyup', function (e) {
+    e.key === 'Enter' || e.key === 'Escape' ? toggleSections() : null;
+    e.key === 'Enter' ? showSection(i) : null;
+  });
+  section.addEventListener('mouseenter', function () {
+    insideSection = true;
+  });
+  section.addEventListener('mouseleave', function () {
+    insideSection = false;
+  });
+});
+document.addEventListener('click', function (event) {
+  if (!insideSection) {
+    itemBodies.forEach(function (item) {
+      item.classList.remove('showHide');
+    });
+  }
 });
 //# sourceMappingURL=app.js.map
