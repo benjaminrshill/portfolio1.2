927f70221d28b616b7db05805379b017a2794431
brownspunbear <benjaminrshill@hotmail.com>
2020-11-16 07:02:29 +0000

    Edit descs

--- app/js/dist/sections.js
+++ app/js/dist/sections.js
@@ -3,25 +3,25 @@
 var items = [[{
   "title": "Item One",
   "miniDesc": "Mini description",
-  "desc": "If rubbin' frozen dirt in your crotch is wrong, hey I don't wanna be right.\n" + "What are you hacking off? Is it my torso?! 'It is!' My precious torso!\n" + "Humans dating robots is sick. You people wonder why I'm still single?\n",
+  "desc": "Those labours which belong to the various branches of the mathematical sciences, although on first consideration they seem to be the exclusive province of intellect, may, nevertheless, be divided into two distinct sections; one of which may be called the mechanical, because it is subjected to precise and invariable laws, that are capable of being expressed by means of the operations of matter; while the other, demanding the intervention of reasoning, belongs more specially to the domain of the understanding.\n" + "If rubbin' frozen dirt in your crotch is wrong, hey I don't wanna be right.\n" + "What are you hacking off? Is it my torso?! 'It is!' My precious torso!\n" + "Humans dating robots is sick. You people wonder why I'm still single?\n",
   "image": "https://images.unsplash.com/photo-1593604977097-8ee5f2fda183?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80",
   "link": "https://example.com"
 }], [{
   "title": "Item Two",
   "miniDesc": "Mini description",
-  "desc": "Me fail English? That's unpossible.\n" + "I'm allergic to bee stings. They cause me to, uh, die. Ahoy hoy? What good is money if it can't inspire terror in your fellow man?\n" + "Dear Mr. President, There are too many states nowadays. Please, eliminate three.\n" + "P.S. I am not a crackpot.",
+  "desc": "This admitted, we may propose to execute, by means of machinery, the mechanical branch of these labours, reserving for pure intellect that which depends on the reasoning faculties. Thus the rigid exactness of those laws which regulate numerical calculations must frequently have suggested the employment of material instruments, either for executing the whole of such calculations or for abridging them; and thence have arisen several inventions having this object in view, but which have in general but partially attained it.\n" + "Me fail English? That's unpossible.\n" + "I'm allergic to bee stings. They cause me to, uh, die. Ahoy hoy? What good is money if it can't inspire terror in your fellow man?\n" + "Dear Mr. President, There are too many states nowadays. Please, eliminate three.\n" + "P.S. I am not a crackpot.",
   "image": "https://images.unsplash.com/photo-1501696461415-6bd6660c6742?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80",
   "link": "http://www.example.com"
 }], [{
   "title": "Item Three",
   "miniDesc": "Mini description",
-  "desc": "And now I'm a fish called \"Father\" With gills type dizzy, Blowing liquid lullabies through the spine of time, I'm\n" + "Certain of Saturn's rivers, and all else is fact, So baptize me in the stars And wrap me in nighttime, moon blue\n" + "Pupil my sight with orange balls of light And echo my plight Through the corridors of metaphor\n" + "What else are we living for, if not to create Fiction and rhyme?\n" + "My purpose is to make my soul Rhyme with my mind over matter\n" + "Minds create matter minds create fiction\n" + "As a matter of fact as if, matter were fact Matter is fact\n" + "So spirit must be fiction, science fiction, art fiction, meta fiction",
+  "desc": "And now I'm a fish called \"Father\" With gills type dizzy, Blowing liquid lullabies through the spine of time, I'm\n" + "Certain of Saturn's rivers, and all else is fact, So baptize me in the stars And wrap me in nighttime, moon blue\n" + "Pupil my sight with orange balls of light And echo my plight Through the corridors of metaphor\n" + "What else are we living for, if not to create Fiction and rhyme?\n" + "My purpose is to make my soul Rhyme with my mind over matter",
   "image": "https://images.unsplash.com/photo-1504681869696-d977211a5f4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1162&q=80",
   "link": "https://example.com"
 }], [{
   "title": "Item Four",
   "miniDesc": "Mini description",
-  "desc": "Wingless angels Stroll atop shapeless cotton balls\n" + "With halos in your syringe Celestial ground is found, broken, " + "Exposing a bottomless depth, Where heartless spines awake to devour the small piece of your soul that's left\n" + "The night breathes, but light's choking, Darkness occupies the throne where poems are persecuted, The purity of time diluted\n" + "Life's an origami box and I'm hidden within the fold, So when the yarn unravels I won't be caught by surprise\n" + "And as society's fabric of orthodoxies dismantle I'll see you embracing the pentagram within this crucifix disguise",
+  "desc": "For instance, the much-admired machine of Pascal is now simply an object of curiosity, which, whilst it displays the powerful intellect of its inventor, is yet of little utility in itself. Its powers extended no further than the execution of the first four operations of arithmetic, and indeed were in reality confined to that of the first two, since multiplication and division were the result of a series of additions and subtractions. The chief drawback hitherto on most of such machines is, that they require the continual intervention of a human agent to regulate their movements, and thence arises a source of errors; so that, if their use has not become general for large numerical calculations, it is because they have not in fact resolved the double problem which the question presents, that of correctness in the results, united with economy of time.",
   "image": "https://images.unsplash.com/photo-1536226957245-c83ec577a82e?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   "link": "https://www.example.com"
 }], [{
