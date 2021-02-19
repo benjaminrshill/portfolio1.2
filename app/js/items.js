c55ee24d53fabe941cea77d2aef7cea82e828ab8
brownspunbear <benjaminrshill@hotmail.com>
2021-02-19 22:31:07 +0000

    edit items

--- app/js/items.js
+++ app/js/items.js
@@ -202,7 +202,6 @@ const items = [
             "desc":
                 "I am an enthusiastic full stack junior web developer.\n" +
                 "After some years following the path of an artist, while building my own gallery website (and deciding to learn some JavaScript to get it behaving the way I wanted), I discovered a real enjoyment of coding.\n" +
-                "Where problem solving, clean code, and UI & graphic design merge into one dynamic whole, there is always something to learn and be fascinated by, and so much potential for innovation, interactivity, and an incredible user experience.\n" +
                 "In the past year, both before and since joining iO Academy as a full-stack student, I’ve greatly enjoyed learning both JavaScript and PHP and working alongside my teammates on various group projects.\n" +
                 "In my life outside of code, I paint, and spend a lot of time attempting various physical pursuits, including juggling, strength training, running, and bouldering.  Fluent in Chinese, I spent five years living in Beijing studying Mandarin and tattooing.",
             "image": "./app/images/me.jpg",
